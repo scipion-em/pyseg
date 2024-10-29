1d1949df7a80e5dec365bb945809a32019450c39
JorMaister <jorgejjs86@gmail.com>
2024-10-29 12:25:22 +0100

    small fix

--- pyseg/__init__.py
+++ pyseg/__init__.py
@@ -126,7 +126,8 @@ class Plugin(pwem.Plugin):
     @classmethod
     def _genCmdToInstallCFitsIO(cls, thirdPartyPath, pySegDir, controlFile):
         CFITSIO_BUILD_PATH = join(pySegDir, '%s_build' % CFITSIO)
-        installationCmd = 'conda activate %s && ' % PYSEG_ENV_NAME
+        installationCmd = cls.getCondaActivationCmd()
+        installationCmd += 'conda activate %s && ' % PYSEG_ENV_NAME
         installationCmd += 'tar zxf %s --directory=%s && ' % (join(thirdPartyPath, 'cfitsio_3.380.tar.gz'), pySegDir)
         installationCmd += 'cd %s && ' % join(pySegDir, CFITSIO)
         installationCmd += 'mkdir %s && ' % CFITSIO_BUILD_PATH
@@ -141,7 +142,8 @@ class Plugin(pwem.Plugin):
         # Remove old disperse included in pyseg distribution
         zipName = 'master.tar.gz'
         buildPath = join(pySegDir, f'{DISPERSE}-master')
-        installationCmd = 'conda activate %s && ' % PYSEG_ENV_NAME
+        installationCmd = cls.getCondaActivationCmd()
+        installationCmd += 'conda activate %s && ' % PYSEG_ENV_NAME
         installationCmd += 'cd %s && rm -rf disperse* && ' % pySegDir
         # Get the latest disperse
         installationCmd += 'wget https://github.com/thierry-sousbie/DisPerSE/archive/refs/heads/%s && ' % zipName
